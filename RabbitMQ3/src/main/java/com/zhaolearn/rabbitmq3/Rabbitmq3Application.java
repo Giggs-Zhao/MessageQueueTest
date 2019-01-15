f24870c9f42a48b7bd22eaa54728413fd4c45b82
何浩钊 <hehaozhao@foreveross.com>
2019-01-15 16:09:51 +0800

    RabbitMQ3，使用key进行发送

---
+++ RabbitMQ3/src/main/java/com/zhaolearn/rabbitmq3/Rabbitmq3Application.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.rabbitmq3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rabbitmq3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rabbitmq3Application.class, args);
+    }
+
+}
+
---
+++ RabbitMQ3/src/test/java/com/zhaolearn/rabbitmq3/Rabbitmq3ApplicationTests.java
@@ -0,0 +1,20 @@
+package com.zhaolearn.rabbitmq3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Rabbitmq3ApplicationTests {
+    @Autowired
+    private Sender sender;
+    @Test
+    public void contextLoads() {
+        sender.send();
+    }
+
+}
+
