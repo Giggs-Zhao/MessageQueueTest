b7750c5b8b3221b60d19a4b31e33d6ad2e5b99ff
何浩钊 <hehaozhao@foreveross.com>
2019-01-16 09:50:05 +0800

    RabbitMQ4，延时消息队列演示初始化

---
+++ RabbitMQ4/src/main/java/com/zhaolearn/rabbitmq4/RabbitMq4Application.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.rabbitmq4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMq4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMq4Application.class, args);
+    }
+
+}
+
---
+++ RabbitMQ4/src/test/java/com/zhaolearn/rabbitmq4/RabbitMq4ApplicationTests.java
@@ -0,0 +1,19 @@
+package com.zhaolearn.rabbitmq4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitMq4ApplicationTests {
+//    @Autowired
+//    private Sender sender;
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
