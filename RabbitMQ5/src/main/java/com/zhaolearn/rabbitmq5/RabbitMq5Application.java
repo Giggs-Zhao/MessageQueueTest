fdc98a1dfd6c518f0b94760152cc436fe5bb814b
何浩钊 <hehaozhao@foreveross.com>
2019-01-16 11:13:54 +0800

    RabbitMQ5，延时消息队列(DLX)演示初始化

---
+++ RabbitMQ5/src/main/java/com/zhaolearn/rabbitmq5/RabbitMq5Application.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.rabbitmq5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMq5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMq5Application.class, args);
+    }
+
+}
+
