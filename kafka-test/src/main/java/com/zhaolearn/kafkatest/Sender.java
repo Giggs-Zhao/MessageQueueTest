aab29836892ab4dc4a8ecbcaa183c8a2e16eb7ee
何浩钊 <hehaozhao@foreveross.com>
2019-01-30 11:50:34 +0800

    Kafka生产者：初始化

---
+++ kafka-test/src/main/java/com/zhaolearn/kafkatest/Sender.java
@@ -0,0 +1,21 @@
+package com.zhaolearn.kafkatest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sender {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Sender.class);
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    public void send(String name, String message) {
+        LOGGER.info("kafka的消息={}", message);
+        kafkaTemplate.send(name, message);
+    }
+
+
+}
--- kafka-test/src/test/java/com/zhaolearn/kafkatest/KafkaTestApplicationTests.java
+++ kafka-test/src/test/java/com/zhaolearn/kafkatest/KafkaTestApplicationTests.java
@@ -2,15 +2,18 @@ package com.zhaolearn.kafkatest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KafkaTestApplicationTests {
-
+@Autowired
+private Sender sender;
     @Test
     public void contextLoads() {
+        sender.send("test_topic","测试数据");
     }
 
 }
