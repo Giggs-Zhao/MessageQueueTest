1b9dc549cb83233ba79cf3c6536bf504b370721e
何浩钊 <hehaozhao@foreveross.com>
2019-01-30 11:50:10 +0800

    Kafka消费者：初始化

---
+++ kafka-consumer-test/src/main/java/com/zhaolearn/kafkaconsumertest/KafkaConsumerTestApplication.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.kafkaconsumertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerTestApplication.class, args);
+    }
+
+}
+
