3729e63e4da1e5b46c7e00c2e0e8d912fd4dc35a
何浩钊 <hehaozhao@foreveross.com>
2019-01-30 17:08:41 +0800

    Kafka消费者：瞎搞

--- kafka-consumer-test/src/main/java/com/zhaolearn/kafkaconsumertest/Comsumer1.java
+++ kafka-consumer-test/src/main/java/com/zhaolearn/kafkaconsumertest/Comsumer1.java
@@ -4,15 +4,13 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Comsumer {
-    private final static Logger LOGGER = LoggerFactory.getLogger(Comsumer.class);
-//    @KafkaListener(topicPartitions = { @TopicPartition(topic = "test_topic", partitions = { "0"}) },groupId = "test")
-@KafkaListener(topics = {"test_topic2"},groupId = "test")
-    public void listen (ConsumerRecord<?, ?> record){
+public class Comsumer1 {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Comsumer1.class);
+    @KafkaListener(topics = {"test_topic2"},groupId = "test")
+    public void listen (ConsumerRecord<?, ?> record) {
         LOGGER.info(record.toString());
         LOGGER.info("【消费{}主题, 第{}分区, 数据:{}, 偏移量:{}】", record.topic(), record.partition(), record.value(), record.offset());
     }
